355a9b96bfe564d9eae7c987a1890139f8216edc
s18739 <61964983+s18739@users.noreply.github.com>
2020-03-29 22:06:34 +0200

    Tilemap

---
+++ 02_Tilemap/02_tiles/Assets/scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Player;
+    public float cameraDistance = 50.0f;
+
+    void Awake (){
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    void FixedUpdate(){
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+    }
+}
---
+++ 02_Tilemap/02_tiles/Assets/scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 500;
+    public float jump = 200;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+public bool Right = true;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown("space") && (isGrounded == true)) {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+        }
+
+        if(xDisplacement<0 && Right) Flip();
+        if(xDisplacement>0 && !Right) Flip();
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        isGrounded = true;
+
+    }
+
+    void Flip (){
+        Right = !Right;
+        transform.Rotate(Vector2.up * 180);
+    }
+}
